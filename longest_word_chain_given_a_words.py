e591803c31c26ef6b3dbd5011726438abff6e4d7
samuelmao415 <samuelmao415@gmail.com>
2019-06-14 18:11:43 -0400

    new challenge

---
+++ longest_word_chain_given_a_words.py
@@ -0,0 +1,52 @@
+#Given a dictionary (list) of "words", find the longest chain possible
+#where: you can remove any letter from the word and still be in the dictionary.
+import re
+
+dict = {"nyc", "datascience", "python", "coding", "sqlanddatabase", "academy"}
+str = "nycdatascienceacademyisaplacefordatasicne"
+
+def find_word_chain(dict, str):
+    matched_word = []
+    for word in dict:
+        str_len = len(str)
+        word_len = len(word)
+
+        str_index = 0
+        word_index = 0
+
+        #find how many matches are there for each word
+        while (str_index < str_len and word_index < word_len):
+            if word[word_index] == str[str_index]:
+                word_index += 1
+            str_index += 1
+        matched_word.append([word_index,word])
+
+    #transform matching words with matched length into dataframe
+    df = pd.DataFrame(matched_word[1:],columns=['len','word'])
+    output = df.loc[df.len == max(df.len)].word.values
+    return(print(output))
+
+find_word_chain(dict, str)
+
+###offical answer:
+def longest(word, word_set, cache):
+    if word not in cache:
+        now = 1
+        for i in range(len(word)):
+            new_word = word[:i] + word[i+1:]
+            if new_word in word_set:
+                current = longest(new_word, word_set, cache)
+                now = max(now, 1 + current)
+        cache[word] = now
+        return cache[word]
+    else:
+        return cache[word]
+
+def longestChain(words):
+    cache = {}
+    word_set = set(words)
+    current_max = 0
+    for word in words:
+        current_max = max(current_max, longest(word, word_set, cache))
+
+    return current_max
