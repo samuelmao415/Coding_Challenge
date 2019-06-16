5b9a593841a4134d495d3495b6015f7674f12141
samuelmao415 <samuelmao415@gmail.com>
2019-06-15 21:39:03 -0400

    added new code

--- longest_word_chain_given_a_words.py
+++ longest_word_chain_given_a_words.py
@@ -1,6 +1,7 @@
 #Given a dictionary (list) of "words", find the longest chain possible
 #where: you can remove any letter from the word and still be in the dictionary.
 
+#match an input string with a given list
 dict = {"nyc", "datascience", "python", "coding", "sqlanddatabase", "academy"}
 str = "nycdatascienceacademyisaplacefordatasicne"
 
@@ -22,6 +23,40 @@ def find_word_chain(dict, str):
     #redo here
     output = matched_word.get(max(matched_word.keys()))
     return(output)
+
+
+
+
+
+
+#match each word from the list
+dict = {"nyc", "datascience", "python", "coding", "sqlanddatabase", "academy",'abvaseudondaj','datainfomanagement'}
+
+def find_word_chain(dict):
+    result = {} #maxium length from each match
+    for str in dict:
+        matched_word = {} #matched length for each word
+        for word in [x for x in dict if x not in str]:
+            str_len = len(str)
+            word_len = len(word)
+
+            str_index = 0
+            word_index = 0
+            #find how many matches are there for each word
+            while (str_index < str_len and word_index < word_len):
+                if word[word_index] == str[str_index]:
+                    word_index += 1
+                str_index += 1
+            matched_word.update({word_index:word})
+        #transform matching words with matched length into dataframe
+        result.update({max(matched_word.keys()):[str,matched_word.get(max(matched_word.keys()))]})
+    print(result)
+    result_values = result.get(max(result.keys()))[0] #return the word with the longest chain
+    return(result_values)
+
+chain_list = find_word_chain(dict)
+
+chain_list
 
 
 ###offical answer:
@@ -46,3 +81,5 @@ def longestChain(words):
         current_max = max(current_max, longest(word, word_set, cache))
 
     return current_max
+
+longestChain(['asoidhoiqyewoiuq','asoidhoi','qw','word','words'])
