e501aa2e19cf59a618d3a5d0ac7166004c946206
samuelmao415 <samuelmao415@gmail.com>
2019-07-07 21:31:03 -0400

    longest prefix

---
+++ hackerrank/longest_common_prefix.py
@@ -0,0 +1,24 @@
+strs = ["flower","flow","flight",'flooring']
+strs = ["dog","racecar","car"]
+strs=[]
+longestCommonPrefix(strs)
+
+
+
+def longestCommonPrefix(x):
+    if len(x) ==0:
+        return('') #if empty input
+    #sort by the shortest string and extract the length of that string
+    shortest_str = len(sorted(strs, key= lambda x: len(x))[0])
+    #a reverse while loop from the longest prefix to the shortest prefix
+    counter=shortest_str
+    output_array=[]
+    while counter>0:
+        prefix = [str[:counter] for str in strs]
+        if [prefix[0]] * len(prefix) == prefix: #if the first element equals the whole element
+            return(prefix[0])
+            output.append(prefix)
+        counter = counter -1
+    #output array to check if there is no solution
+    if len(output_array) ==0:
+        return('')
